d7f4937ca49f22283e8133a74a55f23780e1b446
dell <sunilkumard628@gmail.com>
2021-09-25 15:29:59 +0530

    werehouse

--- app/src/main/java/com/example/warehouse/SupplierPage.java
+++ app/src/main/java/com/example/warehouse/SupplierPage.java
@@ -4,14 +4,18 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import org.jetbrains.annotations.NotNull;
 
 public class SupplierPage extends Fragment {
+
+    Button btn_BachToPurchase;
 
     @Nullable
     @org.jetbrains.annotations.Nullable
@@ -21,6 +25,21 @@ public class SupplierPage extends Fragment {
                              @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.supplierpage_activity,container,false);
+
+        btn_BachToPurchase = view.findViewById(R.id.backtoPurchase);
+
+        btn_BachToPurchase.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                FragmentTransaction ft = getFragmentManager().beginTransaction();
+                Home home = new Home();
+                ft.replace(R.id.frame,home);
+                ft.commit();
+
+
+            }
+        });
         return view;
     }
 }
