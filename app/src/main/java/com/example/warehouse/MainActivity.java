e437161ea3488337ccc20d8473b1f107b510a50a
dell <sunilkumard628@gmail.com>
2021-09-27 15:35:18 +0530

    warehouse

--- app/src/main/java/com/example/warehouse/MainActivity.java
+++ app/src/main/java/com/example/warehouse/MainActivity.java
@@ -11,7 +11,6 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -120,8 +119,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 ChatBot chatBot = new ChatBot();
                 ft.replace(R.id.frame, chatBot);
                 ft.commit();
-
-
             }
         });
 
